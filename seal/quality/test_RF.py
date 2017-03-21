f44d35a485bf9b8cf793a4fbe06bbc48628e86e8
David Samu <daavid.samu@gmail.com>
2017-03-21 18:22:51 +0100

    RF test updated.

--- seal/quality/test_RF.py
+++ seal/quality/test_RF.py
@@ -16,11 +16,11 @@ from seal.util import util, ua_query
 from seal.plot import putil
 
 
-
 # Path to RF mapping tables.
+monkeys = ['201', '202']
 fbase = '/home/upf/Research/data/RF mapping/'
-fRF = pd.Series([fbase + '202/202_RF_all_sessions.xlsx'],
-                index=['202'])
+fRF = pd.Series([fbase + mky + '_RF_all_sessions.xlsx' for mky in monkeys],
+                index=monkeys)
 
 
 # %% Misc functions.
@@ -76,11 +76,31 @@ def get_unit_results(u, RFres):
     return uRFres
 
 
-def plot_RF_DS_relation(UA, fdir):
+def intersect_area(d, r1, r2):
     """
-    Plot relation between cleanness of mapping (fit R2) and
-    direction selectivity of units per task.
+    Return the area of intersection of two circles with given distance from
+    each other (d) and radii (r1 and r2).
     """
+
+    if d <= abs(r1-r2):
+        # One circle is entirely enclosed in the other.
+        overlap = np.pi * min(r1, r2)**2
+    elif d >= r1 + r2:
+        # The circles don't overlap at all.
+        overlap = 0
+    else:
+        # There's some non-complete overlap.
+        r12, r22, d2 = r1**2, r2**2, d**2
+        alpha = np.arccos((d2 + r12 - r22) / (2*d*r1))
+        beta = np.arccos((d2 + r22 - r12) / (2*d*r2))
+        gamma = (r12 * np.sin(2*alpha) + r22 * np.sin(2*beta))
+        overlap = r12 * alpha + r22 * beta - 0.5 * gamma
+
+    return overlap
+
+
+def RF_coverage_analysis(UA, fdir):
+    """Relate unit activity to RF coverage."""
 
     # Init.
     stims = ('S1', 'S2')
@@ -95,45 +115,79 @@ def plot_RF_DS_relation(UA, fdir):
     for u in UA.iter_thru():
         # Get RF mapping results.
         uRF_DSres = get_unit_results(u, RFres)
-        x, y = uRF_DSres[['cntr_x', 'cntr_y']]
+        x, y, FWHM = uRF_DSres[['cntr_x', 'cntr_y', 'FWHM']]
+        RF_rad = FWHM / 2  # approximate circular RF radius by half of FWHM.
         for stim in stims:
             # Get DS.
             uRF_DSres[stim+'_mDSI'] = u.DS.DSI.mDS[stim]
-            # Calculate distance of RF center from each stimulus.
+            # Get stim size.
+            stim_sizes = u.TrData[(stim, 'Size')].value_counts()
+            if len(stim_sizes) > 1:
+                warnings.warn(('More than one simulus sizes found in unit: ' +
+                               u.Name + ', using the one with most trials.'))
+            stim_rad = stim_sizes.index[0] / 2  # diameter --> radius
+            uRF_DSres[stim+'_rad'] = stim_rad
+            # Calculate distance and overlap between RF and each stimulus.
             stim_locs = list(u.TrData[(stim, 'Loc')].unique())
             for i, stimloc in enumerate(stim_locs):
+                # Set names.
+                postfix = '_'+str(i+1) if i > 0 else ''
+                dname, cname = ['{}_{}{}'.format(stim, nm, postfix) for nm in
+                                ('dist', 'cover')]
+                # Calc distance.
                 dist = sp.spatial.distance.euclidean([x, y], stimloc)
-                name = stim + '_dist' + ('_'+str(i+1) if i > 0 else '')
-                uRF_DSres[name] = dist
+                uRF_DSres[dname] = dist
+                # Calc coverage.
+                isa = intersect_area(dist, RF_rad, stim_rad)
+                RF_area = np.pi * RF_rad**2
+                uRF_DSres[cname] = isa/RF_area
+            # Get average rate during each stimulus.
+            stim_rates = u.get_prd_rates(stim, add_latency=True)
+            # Mean across trials.
+            uRF_DSres[stim+'_mean_rate'] = float(stim_rates.mean())
+            # Mean to best direction.
+            dir_trs = u.trials_by_param((stim, 'Dir'))
+            max_rate = max([float(stim_rates[trs].mean()) for trs in dir_trs])
+            uRF_DSres[stim+'_max_rate'] = max_rate
         RF_DS_res.append(uRF_DSres)
     RF_DS_res = pd.concat(RF_DS_res, axis=1).T
     RF_DS_res = RF_DS_res.astype(float)
 
-    # Plot RF coverage and DS on regression plot.
+    # Plot RF coverage and rate during S1 on regression plot for each
+    # recording and task.
     tasks = RF_DS_res.index.get_level_values(3).unique()
-    fig, gs, axes = putil.get_gs_subplots(nrow=len(stims), ncol=len(tasks),
-                                          subw=4, subh=4, ax_kws_list=None,
-                                          create_axes=True)
-    colors = sns.color_palette('muted', len(tasks))
-    for istim, stim in enumerate(stims):
-        for itask, task in enumerate(tasks):
-            # Plot regression plot.
-            ax = axes[istim, itask]
-            sDSI = stim+'_mDSI'
-            df = RF_DS_res.xs(task, level=3)
-            sns.regplot('R2', sDSI, df, color=colors[itask], ax=ax)
-            # Set title.
-            r, p = sp.stats.pearsonr(df.R2, df[sDSI])
-            pstr = util.format_pvalue(p)
-            title = '{} {}: r = {:.2f} ({})'.format(task, stim, r, pstr)
-            xlab = 'RF mapping R2'
-            ylab = stim + ' DSI'
-            putil.set_labels(ax, xlab, ylab, title)
+    for vname, ylim in [('mean_rate', [0, None]), ('max_rate', [0, None]),
+                        ('mDSI', [0, 1])]:
+        fig, gs, axes = putil.get_gs_subplots(nrow=len(stims), ncol=len(tasks),
+                                              subw=4, subh=4, ax_kws_list=None,
+                                              create_axes=True)
+        colors = sns.color_palette('muted', len(tasks))
+        for istim, stim in enumerate(stims):
+            for itask, task in enumerate(tasks):
+                # Plot regression plot.
+                ax = axes[istim, itask]
+                scov, sval = [stim + '_' + name for name in ('cover', vname)]
+                df = RF_DS_res.xs(task, level=3)
+                sns.regplot(scov, sval, df, color=colors[itask], ax=ax)
+                # Add stats.
+                r, p = sp.stats.pearsonr(df[sval], df[scov])
+                pstr = util.format_pvalue(p)
+                txt = 'r = {:.2f}, {}'.format(r, pstr)
+                ax.text(0.02, 0.98, txt, va='top', ha='left',
+                        transform=ax.transAxes)
+                # Set labels.
+                title = '{} {}'.format(task, stim)
+                xlab, ylab = [sn.replace('_', ' ') for sn in (scov, sval)]
+                putil.set_labels(ax, xlab, ylab, title)
+                # Set limits.
+                xlim = [0, 1]
+                putil.set_limits(ax, xlim, ylim)
 
-    # Save plot.
-    title = UA.Name
-    ffig = fdir + '.png'
-    putil.save_fig(ffig, fig, title, ytitle=1.1)
+        # Save plot.
+        title = UA.Name
+        fname = '{}_cover_{}.png'.format(util.format_to_fname(UA.Name), vname)
+        ffig = util.join([fdir, vname, fname])
+        putil.save_fig(ffig, fig, title, ytitle=1.1)
 
 
 # %% Exclude units with low RF coverage.
