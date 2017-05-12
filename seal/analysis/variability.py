d68c1960a4bec9972b9b637206195b8d9a4c72e8
David Samu <daavid.samu@gmail.com>
2017-05-12 13:50:52 +0200

    Updated Fano factor calculation.

--- seal/analysis/variability.py
+++ seal/analysis/variability.py
@@ -10,11 +10,16 @@ variability.(Fano factor, noise correlation, etc).
 import numpy as np
 import pandas as pd
 
+from quantities import ms
+
 
 # %% Functions to calculate Fano factor.
 
 def fano_factor(v):
-    """Calculate Fano factor of vector of spike counts."""
+    """
+    Calculate Fano factor of vector of spike counts.
+    IMPORTANT: v should be vector of spike counts, not rates!
+    """
 
     varv, meanv = np.var(v),  np.mean(v)
 
@@ -25,22 +30,25 @@ def fano_factor(v):
     return fanofac
 
 
-def fano_factor_prd(rate):
+def fano_factor_prd(spk_cnt_df):
     """Calculate Fano factor at each time point of rate DataFrame."""
 
-    ff_prd = pd.Series([fano_factor(rate[t]) for t in rate.columns],
-                       index=rate.columns)
+    ff_prd = pd.Series([fano_factor(spk_cnt_df[t])
+                        for t in spk_cnt_df.columns],
+                       index=spk_cnt_df.columns)
 
     return ff_prd
 
 
-def fano_factor_trial_sets(u, trs_ser, nrate, t1s, t2s, ref_ts):
+def fano_factor_trial_sets(u, trs_ser, win_width, t1s, t2s, ref_ts):
     """Calculate Fano factor for each set of trials."""
 
+    nrate = 'R' + str(int(win_width))
     dff = {}
     for name, trs in trs_ser.items():
         rate = u._Rates[nrate].get_rates(trs, t1s, t2s, ref_ts)
-        dff[name] = fano_factor_prd(rate)
+        spk_cnt = rate * (float(win_width.rescale(ms)) / 1000)
+        dff[name] = fano_factor_prd(spk_cnt)
 
     ff = pd.concat(dff, axis=1).T
 
